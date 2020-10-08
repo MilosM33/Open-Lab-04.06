dc451056bd2009bb5c7d212c0713e43185713d3f
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 21:02:48 +0200

    init

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_04._06
 {
     public class Numbers
     {
-        public int[] NoOdds(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
+        public int[] NoOdds(int[] numbers) => numbers.Where(p => p % 2 == 0).ToArray();
+        
     }
 }
